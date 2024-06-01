1d634beb9c099f6febe8fec9b2c38714a3bf186c
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-06-01 14:48:39 +0300

    feat: :memo: create our seperate pages and routing them with react-router

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,9 @@
+function AppLayout() {
+  return (
+    <div>
+      <h1>AppLayout</h1>
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>WorldWise</h1>
+    </div>
+  );
+}
+
+export default HomePage;

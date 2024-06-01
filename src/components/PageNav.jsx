492c529a67344795221d6c5520a8f16fd74f1555
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-06-01 15:33:22 +0300

    style: :art: style page nav component

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,22 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNav() {
   return (
-    <div>
+    <div className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="product">Product</NavLink>
         </li>
         <li>
           <NavLink to="pricing">pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="login" className="cta">
+            Login
+          </NavLink>
         </li>
       </ul>
     </div>

4494a9cd1f7eb17c9834ea67388d2d6925dbf1d1
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-06-01 21:12:45 +0300

    feat: :sparkles: read and set up query string

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -10,11 +10,15 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, date, emoji, id } = city;
+  const { cityName, date, emoji, id, position } = city;
 
+  console.log(position);
   return (
     <li>
-      <Link className={styles.cityItem} to={id}>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <p className={styles.name}>{cityName}</p>
         <time className={styles.date}>({formatDate(date)})</time>
